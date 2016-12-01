eccc5802c92b33c1b5820efdfaf496eead782368
DeeKeilholz <deekeilholz@gmail.com>
2016-12-01 20:22:42 +0000

    adding app jsx sending data to server

--- src/App.jsx
+++ src/App.jsx
@@ -4,85 +4,76 @@ import MessageList from './MessageList.jsx';
 
 
 class App extends Component {
+
   constructor(props) {
     // passes the props to React.component i.e. the parent class of this component
     super(props)
     // sets up the default state for th app
     this.state = {
       title: 'Chatty',
-
-      currentUser: {
-        name: "Bob"
-      },
-      // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-        username: "Bob",
-        content: "Has anyone seen my marbles?",
-        },
-        {
-        username: "Anonymous",
-        content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      currentUser: {name: "Bob"},
+      messages: []
+      // messages coming from the server will be stored here as they arrive
     }
-// end of this.state
   }
 
+  // componentDidMount is called when the App component is first rendered on the page.
 
- // componentDidMount is called when the App component is first rendered on the page. The setTimeout waits 3 seconds and then adds a new message from Michelle to the list of messages.
+  componentDidMount() {
+    console.log("componentDidMount <App />");
 
- //  componentDidMount() {
- //   console.log("componentDidMount <App />");
- //   setTimeout(() => {
- //     console.log("Simulating incoming message");
- //     // Add a new message to the list of messages in the data store
- //     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
- //     const messages = this.state.messages.concat(newMessage)
- //     // Update the state of the app component.
- //     // Calling setState will trigger a call to render() in App and all child components.
- //     this.setState({messages: messages})
- //   }, 3000);
- // }
+    this.webSocket = new WebSocket("ws://localhost:4000");
 
+    this.webSocket.onopen = (event) => {
+      console.log('So friggin connected to the server')
+    }
 
-// function that should receive the message typed in Chatbar and adds it to the database
-// on event call the function (get the message)
-
-submitMessage = (message) => {
-  // define messages
-  // console.log(this.state)
-  let messages = this.state.messages
-  // set up object that receives new message
-  console.log(messages);
-  const newMessage = {
-    username: "Bob",
-    content: message
   }
-  //push new messages into messages array
-  messages.push(newMessage)
-  // have the new state reflect the message
-  this.setState({messages: messages})
-}
+
+  // this.webSocket.onmessage = (fromWS) => {
+  //     const messages = this.state.messages.concat(JSON.parse(fromWS.data))
+  //     console.log(messages)
+  // }
+
+  // function that receveives the message typed in Chatbar and adds it to the database
+  // on event call the function (get the message)
+
+  submitMessage = (message) => {
+    // define messages
+    // console.log(this.state)
+    let messages = this.state.messages
+    // set up object that receives new message
+    console.log(messages);
+    const newMessage = {
+      username: "Bob",
+      content: message
+    }
+    //push new messages into messages array
+    messages.push(newMessage)
+    // have the new state reflect the message
+    this.setState({messages: messages})
+
+
+    const stringMessage = JSON.stringify(newMessage);
+    this.webSocket.send(stringMessage);
+  }
 
 
   render() {
-  console.log("Rendering <App/>")
+    console.log("Rendering <App/>")
 
 
     return (
       <div className="wrapper">
-
         <nav>
           <h1> {this.state.title} </h1>
         </nav>
-
         <MessageList messages={this.state.messages}/>
-        <Chatbar currentUser={this.state.currentUser.name} submitMessage={this.submitMessage} />
-
+        <Chatbar currentUser={this.state.currentUser.name} submitMessage={this.submitMessage} sendNewMessage={this.sendNewMessage} />
       </div>
     )
   }
 }
-  //pass prop that is message callback
+
+//pass prop that is message callback
 export default App;
